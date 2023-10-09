5d27f68c700fe62d5afd9155b476fb9b563107bb
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-09 21:01:04 +0200

    Initial commit

---
+++ solution.js
@@ -0,0 +1,36 @@
+//1
+const str = "string";
+console.log(str);
+
+//2
+console.log(str[1]);
+console.log(str[5]);
+
+//3
+const firstName = "Maria";
+
+//4
+const age = 30;
+
+//5
+const year = 2023;
+
+//6
+console.log(`${firstName} is ${age} in ${year}`);
+
+//7
+const isMarried = true;
+
+//8
+console.log(`${firstName} is married: ${isMarried}`);
+
+//9
+const programmingLanguage = "JavaScript";
+const isFun = true;
+console.log(`${programmingLanguage} is true: ${isFun}`);
+
+//10
+console.log(`${programmingLanguage[0]} ${programmingLanguage[4]}`);
+
+//11
+console.log(programmingLanguage.length);
